8c1cb695f7b24d022654a255fbdafdb405dcc6d6
misterlengzzz <62778032+misterlengzzz@users.noreply.github.com>
2021-02-20 08:40:37 +0700

    add db-quiz

---
+++ database/migrations/2021_02_20_013656_create_soals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->id();
+            $table->text('soal');
+            $table->text('jawaban_a');
+            $table->text('jawaban_b');
+            $table->text('jawaban_c');
+            $table->text('jawaban_d');
+            $table->text('jawaban_c');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soals');
+    }
+}
